7da70f5652a76f03971781d1624688c0e0f40ab5
olioli <olafmarek.czest@hotmail.com>
2024-03-27 23:17:08 +0100

    Array v1.0

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 
+#include "Array/Array.h"
+
 int main(int argc, char* argv[])
 {
-    std::cout << "Hello world!\n";
+    Array<int> i;
+
+    i.push_back(1);
+    i.push_back(2);
+    i.push_back(3);
+    i.push_back(4);
+    i.push_back(5);
+
+    for (size_t x = 0; x < i.size(); x++)
+    {
+        std::cout << i[x] << '\n';
+    }
+
+    std::cout << "-------------------------------------" << '\n';
+
+    i.resize(10);
+    
+    for (size_t x = 0; x < i.size(); x++)
+    {
+        std::cout << i[x] << '\n';
+    }
+
+    std::cout << "-------------------------------------" << '\n';
+
+    i.resize(15, 1234567890);
+    
+    for (size_t x = 0; x < i.size(); x++)
+    {
+        std::cout << i[x] << '\n';
+    }
     
     return 0;
 }
