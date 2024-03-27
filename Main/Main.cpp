5028d8b804aee8c7ecce232d71e9a6027dbe643f
olioli <olafmarek.czest@hotmail.com>
2024-03-27 23:21:49 +0100

    Delete things in main methode

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,39 +1,8 @@
 #include <iostream>
 
-#include "Array/Array.h"
-
 int main(int argc, char* argv[])
 {
-    Array<int> i;
-
-    i.push_back(1);
-    i.push_back(2);
-    i.push_back(3);
-    i.push_back(4);
-    i.push_back(5);
-
-    for (size_t x = 0; x < i.size(); x++)
-    {
-        std::cout << i[x] << '\n';
-    }
-
-    std::cout << "-------------------------------------" << '\n';
-
-    i.resize(10);
-    
-    for (size_t x = 0; x < i.size(); x++)
-    {
-        std::cout << i[x] << '\n';
-    }
-
-    std::cout << "-------------------------------------" << '\n';
-
-    i.resize(15, 1234567890);
-    
-    for (size_t x = 0; x < i.size(); x++)
-    {
-        std::cout << i[x] << '\n';
-    }
+    std::cout << "Hello world!\n";
     
     return 0;
 }
