3cf44dc5b1d26547f1d07f91870ea5e11f605087
olioli <olafmarek.czest@hotmail.com>
2024-03-28 17:16:24 +0100

    Delete things in main methode

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,21 +1,8 @@
 #include <iostream>
 
-#include "Node/Node.h"
-
 int main(int argc, char* argv[])
 {
-    auto root = Node("root", nullptr);
-    auto child1 = Node("child", &root);
-    auto child2 = Node("child1", &root);
-    auto child3 = Node("child", &root);
-    auto child4 = Node("child", &child1);
-
-    Array<Node*> found = root.find_node_in_offspring("child");
-
-    for (size_t i = 0; i < found.size(); ++i)
-    {
-        std::cout << found[i]->name << " " << found[i] << '\n';
-    }
+    std::cout << "Hello world!\n";
     
     return 0;
 }
