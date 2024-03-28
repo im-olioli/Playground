a93d82200932c31060db51b848ba4c35a6d08c11
olioli <olafmarek.czest@hotmail.com>
2024-03-28 17:15:10 +0100

    Node v0.999

---
+++ Main/Node/Node.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <string>
+
+#include "../Array/Array.h"
+
+class Node
+{
+public:
+    std::string name;
+    Node* parent;
+    Array<Node*> children;
+
+    Node(std::string name, Node* parent);
+    ~Node();
+    Array<Node*> find_node_in_offspring(std::string name);
+};
