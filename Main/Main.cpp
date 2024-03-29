cf0b2a2bbaead2bad27fa849e2daaa90a346395f
olioli <olafmarek.czest@hotmail.com>
2024-03-29 23:35:49 +0100

    Delete things from Main.cpp

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -1,12 +1,8 @@
 #include <iostream>
 
-#include "TextFormatter/TextFormatter.h"
-
 int main(int argc, char* argv[])
 {
-    Text_formatter::header("Hello world!", Text_formatter::Magenta);
-    
-    std::cout << Text_formatter::color_text("Hello world!\n", Text_formatter::Red, Text_formatter::Cyan);
+    std::cout << "Hello world!\n";
     
     return 0;
 }
